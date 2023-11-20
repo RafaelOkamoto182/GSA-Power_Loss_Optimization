b183b2508c67b70841956f93719fe7ec9a506509
Rafael Okamoto <rafa.mg1@hotmail.com>
2023-11-20 14:20:49 -0300

    cada execucao gera um grafico. Nome automatico para arquivos

--- GSA_Min_Power_Loss/main.m
+++ GSA_Min_Power_Loss/main.m
@@ -21,31 +21,38 @@ clear all;clc
 % MeanChart: The average fitnesses Chart over iterations.
 
  N=50;
- max_it=100;
+ max_it=10;
  ElitistCheck=1; Rpower=1;
  min_flag=1; % 1: minimization, 0: maximization
- number_of_runs = 3;
+ number_of_runs = 2;
 
  save_to_csv = 1; %1:save results and solutions to file, 0: do not save.
- file_name = 'GSA_results_F2.csv'
  results_matrix = [];
 
 
- F_index=2;
+ F_index=1;
 
- for i=1:number_of_runs
+ for iteration=1:number_of_runs
  tic;
 
  [Fbest,Lbest,BestChart,MeanChart]=GSA(F_index,N,max_it,ElitistCheck,min_flag,Rpower);
  elapsed_time = toc;
 
- results_matrix_current_row = [i, N, max_it, elapsed_time, Fbest, Lbest];
+ results_matrix_current_row = [iteration, N, max_it, elapsed_time, Fbest, Lbest];
  results_matrix = [results_matrix; results_matrix_current_row];
+
+ graph_title = ['F_index: ', num2str(F_index),', Run: ', num2str(iteration)];
+ figure
+ plot(BestChart, 'k');
+ title(['\fontsize{12}\bf ',graph_title]);
+ xlabel('\fontsize{12}\bf Iteration');ylabel('\fontsize{12}\bf Power Loss (MW)');
 endfor
 
 Fbest_mean = mean(results_matrix(:,5))
 
 if save_to_csv==1
+   file_name = ['GSA_results_F', num2str(F_index),'_maxit', num2str(max_it),'_NoR',num2str(number_of_runs),'.csv'];
+
   if F_index==1
     csv_headers={'#run','N_iterations','max_it/run','elapsed_time','min_power_loss','VG1', 'VG2', 'VG3', 'VG6', 'VG8', 'Tap4-7', 'Tap4-9', 'Tap5-6', 'Qsh9'};
 
@@ -74,11 +81,3 @@ if save_to_csv==1
   end
 
 end
-
- %problema: aqui ele só vai imprimir o grafico da ultima rodada. Precisa explicitar isso no trabalho ou imprimir mais graficos.
- %semilogy(BestChart,'--k');
- plot(BestChart, 'k');
- title(['\fontsize{12}\bf F',num2str(F_index)]);
- xlabel('\fontsize{12}\bf Iteration');ylabel('\fontsize{12}\bf Best-so-far');
- legend('\fontsize{10}\bf GSA',1);
-
